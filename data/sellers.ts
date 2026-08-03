e38a10f4b50de20c2fd1f0f0d93f50fc70a5e6ec
AssFerj <assisjuniorcam@gmail.com>
2026-08-03 10:52:21 -0300

    feat:update sellers

--- data/sellers.ts
+++ data/sellers.ts
@@ -1,104 +1,111 @@
 export const sellers = [
     {
         id: 1,
-        name: "Luana",
-        image: "/sellers/luana3.png",
+        name: "Luana oliveira",
+        image: "/sellers/luana-oliveira.png",
         location: "Camocim",
         phone: "88997030002"
     },
     {
         id: 2,
+        name: "Patrícia",
+        image: "/sellers/patricia.png",
+        location: "Camocim",
+        phone: "88997030002"
+    },
+    {
+        id: 3,
         name: "Martins",
         image: "/sellers/martins.png",
         location: "Camocim",
         phone: "88921712770"
     },
     {
-        id: 3,
+        id: 4,
         name: "Navegantes",
         image: "/sellers/navegantes.png",
         location: "Camocim",
         phone: "88992985867"
     },
     {
-        id: 4,
+        id: 5,
         name: "Ogerlane",
         image: "/sellers/ogerlane.png",
         location: "Camocim",
         phone: "88999438407"
     },
     {
-        id: 5,
+        id: 6,
         name: "Natrícia",
         image: "/sellers/natricia.png",
         location: "Camocim",
         phone: "88992817133"
     },
     {
-        id: 6,
+        id: 7,
         name: "Vando",
         image: "/sellers/vando.png",
         location: "Camocim",
         phone: "88993284350"
     },
     {
-        id: 7,
+        id: 8,
         name: "César",
         image: "/sellers/cesar.png",
         location: "Camocim",
         phone: "88992017837"
     },
     {
-        id: 8,
+        id: 9,
         name: "Jonas",
         image: "/sellers/jonas.png",
         location: "Camocim",
         phone: "88981234946"
     },
     {
-        id: 9,
+        id: 10,
         name: "Nayane",
         image: "/sellers/nayane1.png",
         location: "Camocim",
         phone: "88994585337"
     },
+    // {
+    //     id: 11,
+    //     name: "Verônica",
+    //     image: "/sellers/vera.png",
+    //     location: "Camocim",
+    //     phone: "88993004617"
+    // },
     {
-        id: 10,
-        name: "Verônica",
-        image: "/sellers/vera.png",
-        location: "Camocim",
-        phone: "88993004617"
-    },
-    {
-        id: 11,
+        id: 12,
         name: "Charline",
         image: "/sellers/charline.png",
         location: "Granja",
         phone: "88997895416"
     },
     {
-        id: 12,
+        id: 13,
         name: "Bia",
         image: "/sellers/nayane.png",
         location: "Granja",
         phone: "88998377956"
     },
+    // {
+    //     id: 14,
+    //     name: "Gean",
+    //     image: "/sellers/gean.png",
+    //     location: "Jijoca",
+    //     phone: "88981308721"
+    // },
     {
-        id: 13,
-        name: "Gean",
-        image: "/sellers/gean.png",
-        location: "Jijoca",
-        phone: "88981308721"
-    },
-    {
-        id: 14,
+        id: 15,
         name: "Silvane",
         image: "/sellers/silvane.png",
         location: "Jijoca",
         phone: "88998603085"
     },
     {
-        id: 15,
+        id: 16,
         name: "Márcia",
         image: "/sellers/marcia.png",
         location: "Jijoca",
